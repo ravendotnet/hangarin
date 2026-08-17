9321be4090c0cafcf81b215607320507b5a51d3d
imnotraven <r00vnox@yahoo.com>
2026-08-17 22:41:37 +0800

    models and admin working properly

--- projectsite/webapp/admin.py
+++ projectsite/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Priority, Category, Task, Note, SubTask
 # Register your models here.
+
+admin.site.register(Priority)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Note)
+admin.site.register(SubTask)
--- projectsite/webapp/models.py
+++ projectsite/webapp/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Priority(BaseModel):
+    #priority_id = models.IntegerField(max_length=15)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Category(BaseModel):
+    #category_id = models.IntegerField(max_length=15)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Task(BaseModel):
+    #task_id = models.IntegerField(max_length=15)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    deadline = models.DateField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("Pending", "Pending"),
+            ("In Progress", "In Progress"),
+            ("Completed", "Completed"),
+        ],
+        default="pending"
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
+
+class Note(BaseModel):
+    #note_id = models.IntegerField(max_length=15)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"Note for {self.task.title}"
+
+class SubTask(BaseModel):
+    #subtask_id = models.IntegerField(max_length=15)
+    parent_task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    status = models.CharField(
+            max_length=20,
+            choices=[
+                ("Pending", "Pending"),
+                ("In Progress", "In Progress"),
+                ("Completed", "Completed"),
+            ],
+            default="pending"
+        )
+
+    def __str__(self):
+        return self.title
+
