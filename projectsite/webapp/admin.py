badf34b6d066cff284128bdff3569c7f13a1404b
imnotraven <r00vnox@yahoo.com>
2026-08-20 23:55:24 +0800

    refactored admin.py

--- projectsite/webapp/admin.py
+++ projectsite/webapp/admin.py
@@ -3,8 +3,40 @@ from django.contrib import admin
 from .models import Priority, Category, Task, Note, SubTask
 # Register your models here.
 
-admin.site.register(Priority)
-admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(Note)
-admin.site.register(SubTask)
+#admin.site.register(Priority)
+#admin.site.register(Category)
+#admin.site.register(Task)
+#admin.site.register(Note)
+#admin.site.register(SubTask)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "deadline", "status", "priority", "category",)
+    search_fields = ("title","description",)
+    list_filter = ("status", "priority", "category",)
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "parent_task_name",)
+    search_fields = ("title",)
+    list_filter =("status",)
+
+    def parent_task_name(self, obj):
+        return obj.parent_task.title
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("task", "content", "created_at",)
+    search_fields = ("content",)
+    list_filter = ("created_at",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
--- projectsite/webapp/models.py
+++ projectsite/webapp/models.py
@@ -9,14 +9,22 @@ class BaseModel(models.Model):
 
 class Priority(BaseModel):
     #priority_id = models.IntegerField(max_length=15)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+            verbose_name = "Priority"
+            verbose_name_plural = "Priorities"
 
     def __str__(self):
         return self.name
 
 class Category(BaseModel):
     #category_id = models.IntegerField(max_length=15)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
