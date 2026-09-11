30e7ddb918a23336ef9e7faf67e5a5dde261b78b
imnotraven <r00vnox@yahoo.com>
2026-09-11 12:53:31 +0800

    successful server run for bootstrap. updates: had some changes with views.py importing webapp, edited urls.py

--- projectsite/webapp/views.py
+++ projectsite/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic.list import ListView 
+from webapp.models import Task 
+ 
+class HomePageView(ListView): 
+    model = Task 
+    context_object_name = 'home' 
+    template_name = "home.html" 
 # Create your views here.
